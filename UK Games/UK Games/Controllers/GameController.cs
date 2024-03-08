3dda73ecdc1b4f1e695ea9c33bc32265ebb19df9
Jacksonnnnn <jhuse01@gmail.com>
2024-03-08 12:22:21 -0500

    Getting started

---
+++ UK Games/UK Games/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UK_Games.Controllers;
+
+public class GameController : Controller
+{
+    // GOING TO A GAME PAGE
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
